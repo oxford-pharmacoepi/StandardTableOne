8567fc8c7f6ebc9837418d6b9a3b1b1c1804c470
catalamarti <marticatalasabate@gmail.com>
2023-07-17 12:58:37 +0100

    Update RunAnalysis.R

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -25,14 +25,30 @@ cdm <- generateConceptCohortSet(cdm, "medications", codelistMedications)
 
 # instantiate conditions
 info(logger, "INSTANTIATE CONDITIONS")
-codelistMedications <- codesFromConceptSet(here("Cohorts", "Medications"), cdm)
-cdm <- generateConceptCohortSet(cdm, "medications", codelistMedications)
+codelistConditions <- codesFromConceptSet(here("Cohorts", "Conditions"), cdm)
+cdm <- generateConceptCohortSet(cdm, "conditions", codelistConditions)
 
 # create table summary
-
+result <- summariseCharacteristics(
+  cohort = cdm$cohort1,
+  ageGroup = list(c(0, 19), c(20, 39), c(40, 59), c(60, 79), c(80, 150)),
+  tableIntersect = list(
+    "Visits" = list(
+      tableName = "visit_occurrence", value = "count", window = c(-365, 0)
+     )
+  ),
+  cohortIntersect = list(
+    "Medications" = list(
+      targetCohortTable = "medications", value = "flag", window = c(-365, 0)
+    ),
+    "Conditions" = list(
+      targetCohortTable = "conditions", value = "flag", window = c(-Inf, 0)
+    )
+  )
+)
 
 # export results
-
+write_csv(result, here(output_folder, "table_one.csv"))
 
 # create zip file
 info(logger, "ZIPPING RESULTS")
